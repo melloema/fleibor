f8e857e53a72305b0b4bf9cad9a77a217d79e215
emi85 <emicam85@hotmail.com>
2014-08-10 03:25:12 -0300

    Mas productos

---
+++ porcelana.php
@@ -0,0 +1,100 @@
+<html lang="en">
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Fleibor Website</title>
+    <style id="holderjs-style" type="text/css"></style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-53cd63e656cc5fc1"></script>
+	</head>
+	<body>
+	  	<?php
+	      include("header.php");
+	    ?>
+	    <div class="container company col-md-offset-2 col-md-8">
+			<div class="events-tittle">
+	        	<a name="title"><h3 class="letter-title">Art&iacute;culos para porcelana en fr&iacute;o</h3></a>
+	      	</div>
+	      	<div class="content company row">
+	      		<div class="">
+	      			<p>Las técnicas del trabajo en porcelana en frío se han ido modificando, debido a eso 
+					todos los años desarrollamos nuevos productos para perfeccionar las técnicas y que usted
+					pueda obtener trabajos mas finos. </p>
+					<br/>
+	      			<p>Nuestros aditivos disponibles para usted son los siguientes:</p>
+					<br/>
+					<p>NUEVA LINEA: FULGORADOS Pigmento en polvo con brillo metálico,
+					un nuevo concepto para pintar  su artesanía, aplicar en polvo sobre 
+					la superficie seca para lograr tonos esfumados. Para encontrar todo su 
+					brillo aplicar sobre la superficie de su modelo cera en pasta de piso y luego el polvo.</p>
+					<p> También se lo puede aplicar usando como medio barniz acrílico brillante 
+					o goma laca disuelta encontrando así aplicación colores plenos 
+					con reflejos nacarados y puntos destellantes. </p>
+					<p> Anímese a encontrar en esta línea otra forma de pintar su artesanía con efectos inéditos.</p>
+					<p> Tonos: Amarillo, Azabache, Azul, Blanco, Cobre, Marrón, Naranja, Rojo, Verde, Violeta.</p>
+					<br/>
+					<p>NOVEDAD! FULGOR MÁGICO Novedoso producto hecho para dar un mágico brillo a superficies 
+					que se tornaran con efectos iridiscentes, brillantes o charolados. Se lo aplica ya sea en 
+					polvo o dispersándolo con alcohol, barniz acrílico brillante o goma laca. Para realizar el 
+					efecto de iluminación en los ojos de la figura modelada se recomienda aplicar el producto 
+					adhiriéndolo a la superficie con adhesivo vinílico encontrando así el efecto deseado.</p>
+					<p>Tonos: Blanco, Celeste, Cobre, Dorado, Rojo, Verde y Violeta.</p>
+					<br/>
+					<p>NOVEDAD! PERFUME PARA PORCELANA EN FRÍO Aplicando una o dos gotas del 
+					producto en la figura o modelo ya terminado se logra que adquiera mayor realismo. Ver Listado</p>
+					<br/>
+					<p>NOVEDAD! COLORANTES PARA AERÓGRAFO Ahora puede pintar su trabajo en azúcar o 
+					porcelana en frío con la nueva línea de colorantes para aerógrafos Fleibor, las 
+					superficies lucirán aterciopeladas con este producto. </p>
+					<p>Colores: Amarillo, Azul, Azul O, Marrón, Naranja, Negro, Rojo A, Rojo E, Rojo P, Rosa, Verde y Violeta.</p>
+					<p>Encuentre en este producto los cálidos tonos que su artesanía merece. </p>
+					<br/>
+					<p>COLORANTES EN PASTA Ponemos a su alcance la línea más extensa del mercado de 
+					total definición. Encuentra su aplicación en el coloreado de cualquier masa para
+					modelado (logrando tonos intensos y definidos), a saber: pasta de goma, masa de
+					porcelana en frío, papel machet, etc. Al estar elaborados con colorantes alimenticios 
+					se convierte en un producto no toxico y de máxima concentración en el poder de teñido. 
+					Lanzamiento de nuevos colores: <a href="#">Ver Colores</a></p>
+					<br/>
+					<p>PORCE CLEAR blanquea la pasta de porcelana y aumenta su transparencia.
+					Se agrega a la pasta de porcelana lista para usar en una proporción de 2 gotas por cada 100g.</p>
+					<br/>
+					<p>BRILLOS (simil Glitter) las ventajas de polvos mas finos y brillantes, 
+					cambiando de color según el ángulo de incidencia de la luz. Colores: azul, 
+					estrellas, multicolor, oro, plata, rojo, rosa, tornasolado, verde.</p>
+					<br/>
+					<p>DILUYENTE COL. PORCELANA  </p>
+					<br/>
+					<p>ILUMINADOR polvo superfino para lograr reflejos metálicos. De esta 
+					línea se conocía por tradición el polvo de nácar, nosotros hemos desarrollado 
+					distintos reflejos metalicos NACARADOS: polvo de nácar, azul, dorado, rojo, violeta y verde.</p>
+					<br/>
+					<p>DILIUM diluyente ecológico para oleos. Reemplaza en su uso a la esencia de 
+					trementina, posee una fragancia cítrica y barre eficazmente los oleos. Secado rápido. </p>
+					<br/>
+					<p>POLVOS PARA PÉTALOS para lograr colores esfumados se aplican en seco con un pincel o con los dedos.</p>
+					<p>Para lograr colores plenos, se aplican con un pincel mojado previamente en alcohol 
+					logrando un secado instantaneo.Producto no comestible y no toxico. Listado de Polvos para petalos</p>
+					<br/>
+	      		</div>
+	      	</div>
+			<br>
+
+
+			<a href="productos.php#title">Volver</a>
+      		<hr class="featurette-divider">
+			<?php
+				include("footer.php");
+			?>
+	    </div>
+	</body>
+</html>
