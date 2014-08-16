1f234c2df90fd29519f5ecdd80485ab13c304937
emi85 <emicam85@hotmail.com>
2014-08-16 03:41:13 -0300

    recetas

---
+++ recetas.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Recetas</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav" id="menu">
+                <li class="sidebar-brand">
+
+                        <i><b>RECETAS NUEVAS</i></b>
+
+                </li>
+                <li id="link1">
+                    <a href="#">Bombones de Frutas “Fleibor</a>
+                </li id="link2">
+                <li>
+                    <a href="#">Bombones De Crema Paris</a>
+                </li id="link3">
+                <li>
+                    <a href="#">Torta Galesa Express</a>
+                </li>
+                <li id="link4">
+                    <a href="#">Cubierta Wilton Para Tortas </a>
+                </li>
+                <li id="link5">
+                    <a href="#">Licor Bailey</a>
+                </li>
+                <li id="link6">
+                    <a href="#">Licor Crema de Café & Cacao</a>
+                </li>
+                <li id="link7">
+                    <a href="#">Almíbar Básico</a>
+                </li>
+				<li id="link8">
+                    <a href="#">Rosca Alemana De Rosas</a>
+                </li>
+				<li id="link9">
+                    <a href="#">Crema Pastelera Espesa</a>
+                </li>
+                <li class="sidebar-brand"><i><b>RECETAS ANTERIORES</b></i></li>
+				<li id="link10">
+                    <a href="#">Postre Francis</a>
+                </li>				
+				<li id="link11">
+                    <a href="#">Marmolado de Chocolate, Vainilla y Frutillas</a>
+                </li>				
+				<li id="link12">
+                    <a href="#">Bud&iacute;n de Lim&oacute;n</a>
+                </li>
+				<li id="link13">
+                    <a href="#">Bud&iacute;n Ingl&eacute;s</a>
+                </li>	
+				<li id="link14">
+                    <a href="#">Bud&iacute;n de Chocolate y Dulce de Leche</a>
+                </li>	
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+		
+        <div id="page-content-wrapper">
+		
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+	
+                        <h1 class="text-primary">Recetas por el Chef Daniel Lomazzo</h1>
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><< Ocultar</a>                 
+						<div class="main">
+							<div >
+							<p></p>
+							</div>
+							<hr class="featurette-divider">
+							<div id="page1">
+							<h3>Bombones de Frutas Fleibor</h3> 
+							<br>
+							<p><strong>Ingredientes</strong></p>
+							<p>200 g de puré de manzanas</p>
+							<p>250 g de azúcar común</p>
+							<p>2 cucharadas de pectina en polvo</p>
+							<p>70 g de glucosa</p>
+							<p>1 cucharada de esencia de frutilla Fleibor</p>
+							<p>Colorante en pasta Fleibor a gusto</p>
+							<p>Azúcar común para rebozar C/N</p>
+							<br>
+							<p><strong>Preparación</strong></p>
+<p>Forrar el interior de un molde rectangular chico, o un hermético con una lamina doble de papel manteca. Reservar.</p>
+<p>Colocar dentro de una cacerola el puré de manzana junto con la mitad del azúcar.</p>
+<p>Llevar al fuego, calentar hasta que rompa el hervor y retirar. Adicionar la pectina mezclada con el azúcar restante, la glucosa, la esencia y colorante Fleibor. Unir bien.</p>
+<p>Llevar nuevamente a fuego moderado, y continuar la cocción revolviendo constantemente con una cuchara de madera, hasta alcanzar una temperatura de 110°C. (Utilizar para ver testear un termómetro) Retirar del fuego.</p>
+<p>Volcar la preparación dentro del recipiente preparado, y dejar solidificar a temperatura ambiente.</p>
+<p>Retirar del molde y cortar los bombones cuadrados de 2 x 2, o emplear cortantes pequeños de diferentes formatos. Dejar orear por 24 horas en un lugar fresco y seco. Rebozar con azúcar molido y servir.</p>
+						<br>
+<p><strong>A Saber</strong></p>
+<p>Para poder obtener bombones de otros gusto, seguir las indicaciones de la receta anterior, reemplazando la esencia de frutilla y el colorante Fleibor, por otros sabores.</p>
+<p>Ejemplo: esencia de ananá y colorante en pasta amarillo, esencia de menta y colorante en pasta verde, esencia de naranja y colorante en pasta naranja, etc. 				</p>		
+							
+							
+							</div>
+							<div id="page2">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page3">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page4">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page5">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page6">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page7">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page8">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page9">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page10">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page11">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page12">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page13">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+							<div id="page14">        
+							<h1>ssPage 3</h1>
+							<p>Third section of content.</p>
+							</div>
+						
+						</div>	
+						<br>
+										<a href="productos.php#title">Volver</a>
+				    </div>
+            </div>
+		</div>
+
+    </div>
+
+
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery-1.11.1.js"></script>
+    <script src="js/general.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
