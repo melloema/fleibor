f4efe2a03382d3a5227fafcc4656ea45d86d902e
emi85 <emicam85@hotmail.com>
2014-08-16 04:39:34 -0300

    js y css en BR

--- br/js/general.js
+++ br/js/general.js
@@ -77,4 +77,16 @@ $(function(){
 			}
 		});
 	}
+				$('#menu a').click(function(e){
+					 hideContentDivs();
+					 var tmp_div = $(this).parent().index();
+					 $('.main div').eq(tmp_div).show();
+				  });
+
+				function hideContentDivs(){
+					$('.main div').each(function(){
+					$(this).hide();});
+				}
+				hideContentDivs();
+
 });
